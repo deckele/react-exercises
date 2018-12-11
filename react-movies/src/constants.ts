b15aa0bde60dcdd37e18b19cd1af2e5d6f4187a3
Deckel, Emanuel (ed038p) <ed038p@intl.att.com>
2018-12-11 09:36:42 +0200

    [init] initial commit for react-movies

---
+++ react-movies/src/constants.ts
@@ -0,0 +1,8 @@
+// Top Rated Movies
+// https://api.themoviedb.org/3/movie/top_rated?api_key={api_key}&language=en-US&page=1
+// Search Movies
+// https://api.themoviedb.org/3/search/movie?api_key={api_key}&language=en-US&page=1&include_adult=false
+// Movie Details
+// https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=en-US
+
+export const apiKey = "6723a43a0d554a924752c10b71b5b401";
