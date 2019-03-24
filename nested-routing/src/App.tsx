ba0bb45b022f19bc87db91311e4e67b309e81d7b
Deckel, Emanuel (ed038p) <ed038p@intl.att.com>
2019-03-24 09:45:26 +0200

    initial dynamic routing

---
+++ nested-routing/src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Dynamic Routing!
+      </div>
+    );
+  }
+}
+
+export default App;
