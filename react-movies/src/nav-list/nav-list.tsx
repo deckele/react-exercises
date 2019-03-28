c27e626a0f48a560fb0c25f30bb07b9ce17b9110
Deckel, Emanuel (ed038p) <ed038p@intl.att.com>
2019-03-28 09:33:34 +0200

    [refactor] refactor to new hooks api

--- react-movies/src/nav-list/nav-list.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import { NavListItem } from "./nav-list-item/nav-list-item";
-export const NavList: React.FC = () => (
-    <div className="nav-list">
-        <NavListItem label="List" />
-        <NavListItem label="Info" />
-    </div>);
